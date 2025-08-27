149756c556b918ee890a8235aabf6558555d7948
die666web <manhdkdsth1999@gmail.com>
2025-08-27 17:24:16 +0700

    Update

--- Downloads/react_example/src/components/Mycomponents.js
+++ Downloads/react_example/src/components/Mycomponents.js
@@ -2,11 +2,19 @@
 import React, { Component } from 'react' 
 
 class MyComonent extends Component { // cho phép kế thừa tất cả chức năng component từ react
+    
+    state = {
+        name : "KT" ,
+        age : 20,
+        address : "in the my heart"
+    };
     render(){
         // cho phép mk viết những dòng code JSX hay còn gọi là code html trong 1 file js
         // bình thường thì một ngươi f, 
         return(
-            <div>Yes, MyComonent</div>
+            <div>
+                Tên bạn là {this.state.name} và bạn trong {this.state.address}
+            </div>
         )
     }
 }
