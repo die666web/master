c9573b4dfe49394c9cfc517b2c8b5c7d89ac3637
die666web <manhdkdsth1999@gmail.com>
2025-09-07 15:57:42 +0700

    update Bootstrap

--- Downloads/react_example/src/App.js
+++ Downloads/react_example/src/App.js
@@ -1,45 +1,15 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComonent from './components/Mycomponents';
-import React from 'react';
 
-class App extends React.Component {
-  
+const App = () => {
 
-  render()
-  {
-    return (
-      <div>
-        <MyComonent></MyComonent>
-        
-      </div>
-    )
-  }
-}
+  return (
+    <div className="App">
+      <button class ="btn btn-primart">Button</button>
+      Hello world
+    </div>
 
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Hello World
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//     <div>
-//       <MyComonent></MyComonent>
-//     </div>
     
-//   );
-// }
+  );
+}
 
 export default App;
